57b3c97d9d24e1c9008d24e31b39147401c3acfc
Sanjay Samantaray <32169662+mailboxskumar@users.noreply.github.com>
2018-12-03 16:46:47 +0530

    test

--- src/test/java/public2/AccessModifier3.java
+++ src/test/java/public2/AccessModifier3.java
@@ -3,7 +3,7 @@ package public2;
 import public1.AccessModifier1;
 
 public class AccessModifier3 {
-
+#Access Modifier 
 	public static void main(String[] args) {
 		AccessModifier1 acc1 = new AccessModifier1();
 		System.out.println(acc1.count);
@@ -11,3 +11,4 @@ public class AccessModifier3 {
 	}
 
 }
+
